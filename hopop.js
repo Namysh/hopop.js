5dcc4d8bcb1ddadf884ff7042a0bdefeeb6e3f75
Namysh <34720776+Namysh@users.noreply.github.com>
2019-10-25 15:08:56 +0200

    review

--- hopop.js
+++ hopop.js
@@ -1,87 +1,109 @@
-function hopop(selector, content_function, poos = "bottom"){
-    let firstCharacter = selector.substring(0, 1);
-    let followingCharacters = selector.substring(1, selector.length);
-    let elementArray = [];
-    let posList = ["top", "right", "bottom", "left"];
-    //pos = pos in posList && pos;
-    var pos = posList.indexOf(poos) > -1 && poos;
-    !pos && console.error(`La position "${poos}" n'est pas valide`);
-    switch(firstCharacter){
-        case ".":
-            elementArray =  [...document.getElementsByClassName(followingCharacters)];
-        break;
+function hopop(query, content_function, position = "bottom") {
 
-        case "#":
-            elementArray = [document.getElementById(followingCharacters)];
-        break;
+	// parsing the query in order to recuperate the selector
+	const selector = {
+		type: query.substring(0, 1),
+		identifier: query.substring(1, query.length)
+	};
 
-        case "&":
-            elementArray = [];
-        break;
-    }
+	const elements = [];
 
-    elementArray.forEach( (element) => {
-       
-        // On récupère le contenu du pop (HTML)
-        var content = content_function(element);
-        element.addEventListener("mouseover", (e) => {
-            // On génere un ID aléatoire
-            let id =Math.floor((Math.random() * 10000) + 1);
-            // On attribu l'ID aléatoire à l'attribut aria-describedby
-            e.currentTarget.setAttribute("aria-describedby",id);
-            // On prépare la création du code HTML pour le popup
-            var popover = document.createElement("div");
-            // TODO ADD SPAN ARROW en fonction de la place du popup genre en haut a droite etc
-            // On affecte des attributs au popup    
-            popover.setAttribute("id","popup"+id);
-            popover.setAttribute("class","hopop");
-            popover.innerHTML = content;
-            // On insère le tout à la fin du code HTML
-            document.body.lastElementChild.after(popover);
-            // On place le popup dessous le parent en prenons compte de certains paramètres
-            let currentElementData = e.currentTarget.getBoundingClientRect();
-            let currentPopoverData = popover.getBoundingClientRect();
-            currentElementData.detailledData = window.getComputedStyle(element);
-            // Valeur de tailles et de positions des éléments
-            let get = {
-                currentElementX : currentElementData.left + window.scrollX,
-                currentElementY : currentElementData.top + window.scrollY,
-                currentElementWidth : currentElementData.width,
-                currentElementHeight : currentElementData.height,
-                currentPopoverWidth : currentPopoverData.width,
-                currentPopoverHeight : currentPopoverData.height,
-            
-            };
+	// possible locations
+	const posList = ["top", "right", "bottom", "left"];
 
-            popover.style.top = pos === "bottom" ? get.currentElementY + get.currentElementHeight + 5 + "px" :
-                                pos === "right" ? get.currentElementY + get.currentElementHeight / 2 - get.currentPopoverHeight / 2 + "px" :
-                                pos === "top" ? get.currentElementY - get.currentPopoverHeight - 5 + "px" :
-                                pos === "left" ? get.currentElementY + get.currentElementHeight / 2 - get.currentPopoverHeight/2 + "px" :
-                                " 0 px";
-            //TODO pb si l'image a pas chargé alors le height sera mauvais et du coup ca va mal positionner au debut
-            popover.style.left = pos === "bottom" ? ( get.currentElementX + get.currentElementWidth / 2 - get.currentPopoverWidth / 2 ) >= 0 ?
-                                 (get.currentElementX + (get.currentElementWidth / 2) - (get.currentPopoverWidth / 2 ) + "px") :
-                                 (0 + "px") : 
-                                 pos === "right" ? (get.currentElementX + get.currentElementWidth) + 5 + "px" : 
-                                 pos === "top" ? (get.currentElementX + (get.currentElementWidth / 2) - (get.currentPopoverWidth / 2)) + "px" :
-                                 pos === "left" ?   get.currentElementX - 5   - get.currentPopoverWidth + "px" :
-                                 "0 px";
-            // option popup top ou interieur top, gauche ou interieur gauche etc
-        });
+	// position if declared or "bottom" by default
+	const pos = (posList.includes(position) && position) || "bottom";
 
-        element.addEventListener("mouseout", (e) => {
-            // On récupère l'ID du aria-describedby de l'élément
-            let id = e.currentTarget.getAttribute("aria-describedby");
-            // Si l'ID est valide
-            if(id!=null && id!=""){
-                // On récupère le popup associé
-                let item  = document.getElementById("popup"+id);
-                // On supprime le popup
-                item.parentNode.removeChild(item);
-                // On enlève l'attribut aria-describedby à l'élément
-                e.currentTarget.removeAttribute("aria-describedby");
-            }
-        });
-    });
+	// retrieving the elements
+	switch (selector.type) {
+		case ".":
+			[...document.getElementsByClassName(selector.identifier)].forEach(e => elements.push(e));
+			break;
 
-}    
+		case "#":
+			[document.getElementById(selector.identifier)].forEach(e => elements.push(e));
+			break;
+
+		case "&":
+			[].forEach(e => elements.push(e));
+			break;
+	}
+
+	// foreach element
+	elements.forEach((element) => {
+
+		// content retrieval by the callback
+		const content = content_function(element);
+
+		// event mouseover in order to display the popup
+		element.addEventListener("mouseover", (e) => {
+
+			// generating random id
+			const id = Math.floor((Math.random() * 10000) + 1);
+
+			// setting aria-describedby attribute for identify the elements
+			e.currentTarget.setAttribute("aria-describedby", id);
+
+			// preparing popup
+			var popover = document.createElement("div");
+
+			// TODO : add arrow 
+
+			popover.setAttribute("id", "popup" + id);
+			popover.setAttribute("class", "hopop");
+			popover.innerHTML = content;
+
+			// inserting popup at the end of body
+			document.body.lastElementChild.after(popover);
+
+			// recuperating position data for positioning the popup
+			const currentElementData = e.currentTarget.getBoundingClientRect();
+			const currentPopoverData = popover.getBoundingClientRect();
+
+			let get = {
+				currentElementX: currentElementData.left + window.scrollX,
+				currentElementY: currentElementData.top + window.scrollY,
+				currentElementWidth: currentElementData.width,
+				currentElementHeight: currentElementData.height,
+				currentPopoverWidth: currentPopoverData.width,
+				currentPopoverHeight: currentPopoverData.height,
+
+			};
+
+			popover.style.top = pos === "bottom" ? get.currentElementY + get.currentElementHeight + 5 + "px" :
+				pos === "right" ? get.currentElementY + get.currentElementHeight / 2 - get.currentPopoverHeight / 2 + "px" :
+				pos === "top" ? get.currentElementY - get.currentPopoverHeight - 5 + "px" :
+				pos === "left" ? get.currentElementY + get.currentElementHeight / 2 - get.currentPopoverHeight / 2 + "px" :
+				" 0 px";
+
+			//TODO trouble with the images that haven't loaded
+
+			popover.style.left = pos === "bottom" ? (get.currentElementX + get.currentElementWidth / 2 - get.currentPopoverWidth / 2) >= 0 ?
+				(get.currentElementX + (get.currentElementWidth / 2) - (get.currentPopoverWidth / 2) + "px") :
+				(0 + "px") :
+				pos === "right" ? (get.currentElementX + get.currentElementWidth) + 5 + "px" :
+				pos === "top" ? (get.currentElementX + (get.currentElementWidth / 2) - (get.currentPopoverWidth / 2)) + "px" :
+				pos === "left" ? get.currentElementX - 5 - get.currentPopoverWidth + "px" :
+				"0 px";
+		});
+
+		// mouseout event
+		element.addEventListener("mouseout", (e) => {
+			// recuperation of id in aria-describedby
+			const id = e.currentTarget.getAttribute("aria-describedby");
+
+			if (id != null && id != "") {
+
+				// recuperation of associated popup
+				const item = document.getElementById("popup" + id);
+
+				// deleting popup
+				item.parentNode.removeChild(item);
+
+				// removing aria-describedby attribute
+				e.currentTarget.removeAttribute("aria-describedby");
+			}
+		});
+	});
+
+}
